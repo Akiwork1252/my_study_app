cbb36d666528d3e6dd24036e60b749f626b3fae7
Akiwork1252 <aki1252work@gmail.com>
2025-01-25 20:27:44 +0900

    edit: written test

--- learning_test/urls.py
+++ learning_test/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name = 'learning_test'
 urlpatterns = [
-    path('multiple_choice/<int:learning_goal_id>', views.choice_test_view, name='choice_test'),
-    path('written_test/<int:learning_goal_id>', views.written_test_view, name='written_test'),
+    path('multiple_choice/<int:learning_goal_id>/', views.choice_test_view, name='choice_test'),
+    path('written_test/<int:learning_goal_id>/', views.written_test_view, name='written_test'),
 ]
